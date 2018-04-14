a71a48fa2384520fddb8a8b0d39a47876889b546
wellnitm <wellnitm@lafayette.edu>
2018-04-14 18:37:15 -0400

    syntax error fix

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -41,7 +41,7 @@ timer_init (void)
 {
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
-  list_init(&alarmList)
+  list_init(&alarmList);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
