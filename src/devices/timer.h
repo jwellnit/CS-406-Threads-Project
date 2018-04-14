f7fd1a86658ab826d631afafca02ab9951a6bce2
wellnitm <wellnitm@lafayette.edu>
2018-04-14 15:07:41 -0400

    Alarm struct and wait

--- src/devices/timer.h
+++ src/devices/timer.h
@@ -26,4 +26,19 @@ void timer_ndelay (int64_t nanoseconds);
 
 void timer_print_stats (void);
 
+/* alarm. */
+struct alarm
+  {
+    struct lock condLock        /* Lock that must be acquired for the condition variable */
+    struct condition alarmCond  /* Condition variable to signal waiting threads */
+    int64_t start;              /* Start time of the timer */
+    int64_t ticks;              /* Duration of the timer */
+    struct list_elem elem;      /* elem for alarmList */
+  };
+
+  struct list alarmList;
+
+  static bool alarm_first (const struct list_elem *, const struct list_elem *,
+                          void *);
+
 #endif /* devices/timer.h */
