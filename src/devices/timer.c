87358b0976a0fff82d77f8f39da8f6c88d152411
wellnitm <wellnitm@lafayette.edu>
2018-04-22 20:49:02 -0400

    test3

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -206,12 +206,11 @@ timer_interrupt (struct intr_frame *args UNUSED)
   while (!list_empty(&alarmList)){
     struct alarm *next = list_entry(list_front(&alarmList), struct alarm, elem);
     // if (next->end <= timer_ticks()) {
-    //   sema_up(&next->sema);
-    //   list_pop_front(&alarmList);
+       sema_up(&next->sema);
+       list_pop_front(&alarmList);
     // } else {
     //   break;
     // }
-    list_pop_front(&alarmList);
   }
 
 }
