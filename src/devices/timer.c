f311e402e5c12fbebff7602f7064cd4252b66de9
wellnitm <wellnitm@lafayette.edu>
2018-04-22 20:54:01 -0400

    test4

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -204,7 +204,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
   */
 
   while (!list_empty(&alarmList)){
-    struct alarm *next = list_entry(list_front(&alarmList), struct alarm, elem);
+    struct alarm *next = list_entry(list_begin(&alarmList), struct alarm, elem);
     // if (next->end <= timer_ticks()) {
        sema_up(&next->sema);
        list_pop_front(&alarmList);
