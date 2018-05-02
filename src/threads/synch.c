cba989ea4d64784d0e2dd71d4b9cfd73766e8901
FrancineDennehy <frankie.dennehy@gmail.com>
2018-05-01 21:42:18 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -343,6 +343,6 @@ highest_cond_waiting_priority(struct condition *cond){
 	ASSERT (cond != NULL);
 
 	list_sort(&cond->waiters, priority_sort, NULL); //sort the list
-	return list_front(&cond->waiters->priority);
+	return list_front(&cond->waiters.priority);
 	//return the highest
 }
