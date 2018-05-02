6bb3c7e4af7d5ad2e7f106d3a17397bed4372369
agmullan <33397452+agmullan@users.noreply.github.com>
2018-05-02 16:49:19 -0400

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -140,6 +140,6 @@ int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 static bool priority_sort (const struct list_elem *a_, const struct list_elem *b_, void *aux UNUSED); //added
 //static bool thread_get_lock (void);                                                                   //added
-void priority_donate(struct lock lock*);                                                                          //added
+void priority_donate(struct lock *lock);                                                                          //added
 
 #endif /* threads/thread.h */
