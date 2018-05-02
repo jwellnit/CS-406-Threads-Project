1ff70af208305e7098517f6a32348d414fb76540
agmullan <33397452+agmullan@users.noreply.github.com>
2018-05-02 15:30:49 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -562,7 +562,7 @@ void priority_donate(struct lock *lock){
  	int highest = a->priority; // get the priority of the highest priority thread (based on Frankie's code)
  	printf("%d", highest); // try to see it 
 	
-	cur->thread_set_priority(highest); // reset the priority
+	thread_set_priority(highest); // reset the priority
 
 }//end of priority_donate
 
