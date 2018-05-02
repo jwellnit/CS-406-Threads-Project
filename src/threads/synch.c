6c098096c467ec90cbf8979839d63e4c9073d282
wellnitm <wellnitm@lafayette.edu>
2018-05-01 20:11:59 -0400

    Sema sort on up

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -114,6 +114,7 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters))
+    list_sort(&sema->waiters, priority_sort, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
   sema->value++;
