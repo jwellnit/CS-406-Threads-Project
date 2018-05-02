94fb9718df6c365cd51a6d45ac3ac9cbdf87e798
FrancineDennehy <frankie.dennehy@gmail.com>
2018-05-01 21:55:41 -0400

    Update synch.h

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -43,9 +43,10 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
-int highest_cond_waiting_priority(struct condition *cond); //added for priority donation
 static bool cond_priority_sort (const struct list_elem *a_, 
                            const struct list_elem *b_, void *aux UNUSED); //added to sort 
+int highest_cond_waiting_priority(struct condition *cond); //added for priority donation
+
 
 /* alarm. */
 struct alarm
