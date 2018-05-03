e14239a94cabed25fbbc22ef271431b675e92356
FrancineDennehy <frankie.dennehy@gmail.com>
2018-05-02 20:45:23 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -219,7 +219,9 @@ thread_create (const char *name, int priority,
   thread_unblock (t);
 
   thread_yield(); //added this
-	priority_donate(aux);
+	if(aux != NULL){
+	priority_donate(aux);}
+	
   return tid;
 }
 
