e37c38013440129a2620e25869a5b3ccab2ec882
FrancineDennehy <frankie.dennehy@gmail.com>
2018-05-03 17:20:02 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -574,7 +574,8 @@ priority_donate(struct lock *lock){
 
 	//lock_try_acquire(lock)
 	if(lock_try_acquire(lock)){ //current thread tries to acquire the lock
-	   lock_acquire_int(lock);
+	  	return;
+	  // lock_acquire_int(lock);
 	}else{
 		//disable interrupts here
 		struct thread *holder = lock->holder;
