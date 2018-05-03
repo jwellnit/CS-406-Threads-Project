051bfd76d2096e7782912f6cee86dcac48924db0
wellnitm <wellnitm@lafayette.edu>
2018-05-02 20:24:15 -0400

    typo fixes

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -127,8 +127,8 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) {
-    struct thread t = list_entry (list_pop_front (&sema->waiters);
-    thread_unblock (t, struct thread, elem));
+    struct thread t = list_entry (list_pop_front (&sema->waiters), struct thread, elem);
+    thread_unblock (t);
   }
   sema->value++;
   intr_set_level (old_level);
