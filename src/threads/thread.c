da5bd40d918e2df9a464d549874d54093ed83d99
wellnitm <wellnitm@lafayette.edu>
2018-05-03 22:19:13 -0400

    ignore init priority if mlfq

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -511,8 +511,10 @@ init_thread (struct thread *t, const char *name, int priority)
   t->status = THREAD_BLOCKED;
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
-  t->old_priority = priority;
-  t->priority = priority;
+  if (!thread_mlfqs) {
+    t->old_priority = priority;
+    t->priority = priority;
+  }
   t->magic = THREAD_MAGIC;
   list_push_back (&all_list, &t->allelem);
 }
