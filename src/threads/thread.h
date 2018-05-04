e163a84445136452d2eddc3e7b57a2d937591f15
Yuchen Sun <32922504+lyudmilalala@users.noreply.github.com>
2018-05-04 19:43:51 -0400

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -90,7 +90,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     int old_priority;                     /*save old priority for priority donation*/
-    \ bool donated_to; 
+    bool donated_to; 
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
