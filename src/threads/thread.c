e718e3bc2e3f694a654c59abd4ae2cfb3e362d35
Yuchen Sun <32922504+lyudmilalala@users.noreply.github.com>
2018-05-05 19:45:15 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -365,8 +365,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority)
 {
-  ASSERT(thread_current()->donated_to==true);
-  ASSERT(thread_current()->donated_to==false);
+  printf(thread_current()->lower);
   enum intr_level old_level;
   old_level = intr_disable ();
 
