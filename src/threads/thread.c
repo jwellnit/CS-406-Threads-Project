c8a4faa936a7cdd4a4528dee2d9e451949d2542e
Unknown <frankie.dennehy@gmail.com>
2018-05-05 15:20:19 -0400

    confused by results

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -367,10 +367,11 @@ thread_set_priority (int new_priority)
   old_level = intr_disable ();
 
    //check if a thread is locked
-  if(thread_current()->donated_to != true){
+  if(thread_current()->donated_to == true){
      //if(thread_current()->priority > new_priority){
 
         thread_current()->lower = new_priority;
+        return;
 
      //}
      // else{
