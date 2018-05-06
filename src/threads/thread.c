637113b5bc41b3e048af206cb1c9f5433b4b5de9
wellnitm <wellnitm@lafayette.edu>
2018-05-05 23:11:45 -0400

    attempt 2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -729,3 +729,109 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//sinple exponentiation on integer numbers
+int expi (int base, int exp) {
+  if (exp == 0) {
+    return 1;
+  }
+  int ret = base;
+  for (int i = 1; i < exp; i++) {
+    ret *= base;
+  }
+  return ret;
+}
+
+//initializes the params of the type as well as converts int n to
+//the fpoint type
+bool fpoint_init (struct fpoint *x, int q, int n) {
+  if (q > 30 || n > expi(2, 31-q)-1 || n < -1*expi(2, 31-q)) {
+    return false;
+  }
+  x->q = q;
+  x->f = expi(2, q);
+  x->v = n*x->f;
+  return true;
+}
+
+//converts an int n to a fpoint. fpoint must be initialized first
+bool int_to_fpoint(struct fpoint *x, int n) {
+  if (n > expi(2, 31-x->q)-1 || n < -1*expi(2, 31-x->q)) {
+    return false;
+  }
+  x->v = n*x->f;
+  return true;
+}
+
+// converts fpoint x to an int, rounding towards zero
+int fpoint_to_int_zero (struct fpoint *x) {
+  return x->v/x->f;
+}
+
+//converts fpoint x to an int rounding towards the nearest int
+int fpoint_to_int_nearest (struct fpoint *x) {
+  if (x->v >= 0) {
+    return (x->v + (x->f/2))/x->f;
+  } else {
+    return (x->v - (x->f/2))/x->f;
+  }
+}
+
+//add two fpoints x and y
+bool fpoint_add (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
+  if (x->q != y->q || x->q != res->q) {
+    return false;
+  }
+  res->v = x->v + y->v;
+  return true;
+}
+
+//subtract two fpoints x-y
+bool fpoint_sub (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
+  if (x->q != y->q || x->q != res->q) {
+    return false;
+  }
+  res->v = x->v - y->v;
+  return true;
+}
+
+//multiply two fpoints x and y
+bool fpoint_mult (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
+  if (x->q != y->q || x->q != res->q) {
+    return false;
+  }
+  res->v = (int64_t)x->v * y->v / y->f;
+  return true;
+}
+
+//divide two fpoints x/y
+bool fpoint_div (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
+  if (x->q != y->q || x->q != res->q) {
+    return false;
+  }
+  res->v = (int64_t)x->v * y->f / y->v;
+  return true;
+}
+
+//add int n to fpoint x
+void fpoint_add_int (struct fpoint *x, int n, struct fpoint *res) {
+  res->v = x->v + n*x->f;
+}
+
+//subtract int n from fpoint x
+void fpoint_sub_int (struct fpoint *x, int n, struct fpoint *res) {
+  res->v = x->v - n*x->f;
+}
+
+//multiply int n and fpoint x
+void fpoint_mult_int (struct fpoint *x, int n, struct fpoint *res) {
+  res->v = x->v * n;
+}
+
+//divide fpoint x by int n
+void fpoint_div_int (struct fpoint *x, int n, struct fpoint *res) {
+  res->v = x->v / n;
+}
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -165,106 +165,43 @@ struct fpoint {
 };
 
 //sinple exponentiation on integer numbers
-int expi (int base, int exp) {
-  if (exp == 0) {
-    return 1;
-  }
-  int ret = base;
-  for (int i = 1; i < exp; i++) {
-    ret *= base;
-  }
-  return ret;
-}
+int expi (int base, int exp);
 
 //initializes the params of the type as well as converts int n to
 //the fpoint type
-bool fpoint_init (struct fpoint *x, int q, int n) {
-  if (q > 30 || n > expi(2, 31-q)-1 || n < -1*expi(2, 31-q)) {
-    return false;
-  }
-  x->q = q;
-  x->f = expi(2, q);
-  x->v = n*x->f;
-  return true;
-}
+bool fpoint_init (struct fpoint *x, int q, int n);
 
 //converts an int n to a fpoint. fpoint must be initialized first
-bool int_to_fpoint(struct fpoint *x, int n) {
-  if (n > expi(2, 31-x->q)-1 || n < -1*expi(2, 31-x->q)) {
-    return false;
-  }
-  x->v = n*x->f;
-  return true;
-}
+bool int_to_fpoint(struct fpoint *x, int n);
 
 // converts fpoint x to an int, rounding towards zero
-int fpoint_to_int_zero (struct fpoint *x) {
-  return x->v/x->f;
-}
+int fpoint_to_int_zero (struct fpoint *x);
 
 //converts fpoint x to an int rounding towards the nearest int
-int fpoint_to_int_nearest (struct fpoint *x) {
-  if (x->v >= 0) {
-    return (x->v + (x->f/2))/x->f;
-  } else {
-    return (x->v - (x->f/2))/x->f;
-  }
-}
+int fpoint_to_int_nearest (struct fpoint *x);
 
 //add two fpoints x and y
-bool fpoint_add (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
-  if (x->q != y->q || x->q != res->q) {
-    return false;
-  }
-  res->v = x->v + y->v;
-  return true;
-}
+bool fpoint_add (struct fpoint *x, struct fpoint *y, struct fpoint *res);
 
 //subtract two fpoints x-y
-bool fpoint_sub (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
-  if (x->q != y->q || x->q != res->q) {
-    return false;
-  }
-  res->v = x->v - y->v;
-  return true;
-}
+bool fpoint_sub (struct fpoint *x, struct fpoint *y, struct fpoint *res);
 
 //multiply two fpoints x and y
-bool fpoint_mult (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
-  if (x->q != y->q || x->q != res->q) {
-    return false;
-  }
-  res->v = (int64_t)x->v * y->v / y->f;
-  return true;
-}
+bool fpoint_mult (struct fpoint *x, struct fpoint *y, struct fpoint *res);
 
 //divide two fpoints x/y
-bool fpoint_div (struct fpoint *x, struct fpoint *y, struct fpoint *res) {
-  if (x->q != y->q || x->q != res->q) {
-    return false;
-  }
-  res->v = (int64_t)x->v * y->f / y->v;
-  return true;
-}
+bool fpoint_div (struct fpoint *x, struct fpoint *y, struct fpoint *res);
 
 //add int n to fpoint x
-void fpoint_add_int (struct fpoint *x, int n, struct fpoint *res) {
-  res->v = x->v + n*x->f;
-}
+void fpoint_add_int (struct fpoint *x, int n, struct fpoint *res);
 
 //subtract int n from fpoint x
-void fpoint_sub_int (struct fpoint *x, int n, struct fpoint *res) {
-  res->v = x->v - n*x->f;
-}
+void fpoint_sub_int (struct fpoint *x, int n, struct fpoint *res);
 
 //multiply int n and fpoint x
-void fpoint_mult_int (struct fpoint *x, int n, struct fpoint *res) {
-  res->v = x->v * n;
-}
+void fpoint_mult_int (struct fpoint *x, int n, struct fpoint *res);
 
 //divide fpoint x by int n
-void fpoint_div_int (struct fpoint *x, int n, struct fpoint *res) {
-  res->v = x->v / n;
-}
+void fpoint_div_int (struct fpoint *x, int n, struct fpoint *res);
 
 #endif /* threads/thread.h */
