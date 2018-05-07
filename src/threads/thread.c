e5486a80579334427fae86e5c3f2b5fd075215c2
Yuchen Sun <32922504+lyudmilalala@users.noreply.github.com>
2018-05-07 17:01:47 -0400

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -634,7 +634,7 @@ priority_donate(struct lock *lock){
 
       list_sort(&ready_list, priority_sort, NULL);
       thread_yield();
-      holder->donated_to = true;
+      cur->donated_to = true;
       //lock_try_acquire(lock);
 
       //lock_acquire_int(lock);
