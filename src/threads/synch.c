fb112c88f9d93a5b20911aa70b034806e403b03d
Yuchen Sun <32922504+lyudmilalala@users.noreply.github.com>
2018-05-07 16:24:55 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -249,13 +249,13 @@ lock_acquire_int (struct lock *lock)
 enum intr_level old_level;
 old_level = intr_disable ();
 printf("donated_to holder before sema now is %d\n", lock->holder->donated_to);
-printf("donated_to cur before sema now is %d\n", thread_current()->donated_to);
+//printf("donated_to cur before sema now is %d\n", thread_current()->donated_to);
 sema_down (&lock->semaphore);
 lock->holder = thread_current ();
 printf("old priority holder after sema now is %d\n", lock->holder->old_priority);
-printf("old priority cur after sema now is %d\n", thread_current()->old_priority);
+//printf("old priority cur after sema now is %d\n", thread_current()->old_priority);
 printf("donated_to holder after sema now is %d\n", lock->holder->donated_to);
-printf("donated_to cur after sema now is %d\n", thread_current()->donated_to);
+//printf("donated_to cur after sema now is %d\n", thread_current()->donated_to);
 intr_set_level (old_level);
 }
 
